82446234cbce9b13de78fc5227b9b4ba130b1eda
wuhezhen <1104712636@qq.com>
2018-06-26 08:57:28 +0800

    登录页校验完成,首页静态页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -11,3 +11,34 @@ $(document).ajaxStop(function () {
     NProgress.done();
   },500)
 });
+
+
+//公共功能
+$(function() {
+  //1.左侧二级菜单切换显示
+  $('.lt_aside .category').click(function () {
+    $('.lt_aside .child').stop().slideToggle();
+  });
+  //2.左侧整个侧边栏显示隐藏功能
+  $('.lt_topbar .icon_menu').click(function() {
+    $('.lt_aside').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+  });
+  
+  //3.点击头部退出按钮  显示退出模态框
+  $('.lt_topbar .icon-logout').click(function () {
+    //显示模态框
+    $('#logoutModal').modal("show");
+    
+  });
+  
+  //4.点击模态框中的退出按钮  需要进行退出操作(ajax)
+  $('#lougoutBtn').click(function() {
+    //发送ajax请求
+    
+  })
+  
+  
+  
+});
